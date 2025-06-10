04103a332bd4c3b9b5b0544dfc43f5bdd2404188
Lucas <lk7750785@outlook.com>
2025-06-09 21:51:46 -0300

    parâmetros url

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -10,3 +11,22 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar itens do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+
+  console.log(elemento);
+}
+
+if (parametros) {
+  parametros.forEach(ativarProduto);
+}
+
+console.log(parametros);
